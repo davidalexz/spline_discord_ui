62e3c46f03b87fc9d4ee4e4d5aee20a2f57081dc
Alex David <alexz.chimi@gmail.com>
2024-02-07 20:25:08 +0200

    first comit

---
+++ Components/SplineUI.jsx
@@ -0,0 +1,9 @@
+import Spline from '@splinetool/react-spline';
+
+export default function SplineUI() {
+    return (
+        <>
+            <Spline scene="https://prod.spline.design/284z7cRH687tCxdz/scene.splinecode" />
+        </>
+    );
+}
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import SplineUI from '../Components/SplineUI';
+import './App.css';
+
+export default function App() {
+    return (
+        <>
+            <div className="spline-ui">
+                <SplineUI />
+            </div>
+        </>
+    );
+}
